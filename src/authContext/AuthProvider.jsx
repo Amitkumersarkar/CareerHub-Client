f2fdd960c7765e6036f165cc14e5e900e34e6989
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-04-04 17:09:30 +0600

    updated auth

--- src/authContext/AuthProvider.jsx
+++ src/authContext/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import auth from "../firebase/firebase.config";
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
@@ -12,12 +12,26 @@ const AuthProvider = ({ children }) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
+    // create observer
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+            console.log('state captured', currentUser);
+        })
+        // return memory
+        return () => {
+            unSubscribe();
+        }
+
+    }, [])
     const authInfo = {
         user,
         loading,
         setUser,
         setLoading,
         createUser,
+
 
     }
     return (
