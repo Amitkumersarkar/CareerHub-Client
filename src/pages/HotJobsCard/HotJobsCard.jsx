3b268a01c907d4759c0b37eb6f23c8d423f1b726
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-04-10 00:23:25 +0600

    added new file

---
+++ src/pages/HotJobsCard/HotJobsCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HotJobsCard = () => {
+
+    return (
+        <div>
+            {
+
+            }
+        </div>
+    );
+};
+
+export default HotJobsCard;
--- src/pages/Hotjobs/HotJobs.jsx
+++ src/pages/Hotjobs/HotJobs.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
+import HotJobsCard from "../HotJobsCard/HotJobsCard";
 
 const HotJobs = () => {
+    const [jobs, setJobs] = useState();
+
+    useEffect(() => {
+        fetch('http://localhost:4000/jobs')
+            .then(res => res.json())
+            .then(data => setJobs(data));
+    }, [])
 
     return (
         <div>
-
+            <div>
+                {
+                    jobs.map(job => <HotJobsCard key={job._id} job={job}></HotJobsCard>)
+                }
+            </div>
         </div>
     );
 };
