1d9cddc585be6ba11c174fb7fed59be9111ed172
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-04-10 00:14:26 +0600

    added hotjobs sections

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import HotJobs from "../Hotjobs/HotJobs";
 
 const Home = () => {
 
     return (
         <div>
             <Banner></Banner>
+            <HotJobs></HotJobs>
         </div>
     );
 };
